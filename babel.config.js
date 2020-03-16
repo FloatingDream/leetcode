8822551989fde79235b3a1c206a84d5ff1ff4958
ZYC <zyczhangyucheng@outlook.com>
2020-03-16 23:16:21 +0800

    init

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+// babel.config.js
+// eslint-disable-next-line no-undef
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+};
